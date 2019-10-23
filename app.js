d451c911d4bee4e2057d11b3098ca06081ff2fbc
georg.luik <georg.luik@khk.ee>
2019-10-23 16:50:26 +0300

    muutujate initsialiseerimine jadefineerimine

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-// Logid konsoolile
+// muutujad
 
-// logi
-console.log('123')
+// var
+var nimi = 'Georg'; // defineerimine
+console.log(nimi);
+nimi = 'Jok'; // väärtuse omistamine
+console.log(nimi);
 
-// tabel
-console.table({a :1, b : 2});
+// muutujate initsialiseerimine
+var tervitus;
+console.log(tervitus);
+tervitus = 'Tere tulemast';
+console.log(tervitus);
 
-// veateade
-console.error('See on mingi viga');
+// liitnimede kasutamine
+var eesNimi = 'Georg'; // Camel Stiil
+var ees_nimi = 'Georg'; // PHP Stiil
+var EesNimi = 'Georg'; // Pascal Stiil
+var eesnimi = 'Georg';
 
-// märkused
-console.warn('See on märkus');
+// let - muutuja
+let pereNimi = 'Luik';
+console.log(pereNimi);
+pereNimi = 'Luk';
+console.log(pereNimi);
 
-// ajakulu kontroll
-console.time('Test');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-    console.log('mingi tegevus');
-console.timeEnd('Test');
+// const - konstant
+const minuEesNimi = 'Georg';
+console.log(minuEesNimi);
 
-// kustutamine
-console.clear();
+
+// const kasutamise näide
+const inimene = {nimi : 'Georg', vanus : 10};
+console.table(inimene);
+inimene.vanus = 11;
+console.table(inimene);
+
+const arvud = [1, 2, 3, 4, 5];
+console.log(arvud);
+arvud.push(6);
+console.log(arvud);
