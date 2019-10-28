e0fc5c2277ac9f2cfe07872236808a2b8161a92e
georg.luik <georg.luik@khk.ee>
2019-10-28 11:37:29 +0200

    tsükkel sorteerimine

--- app.js
+++ app.js
@@ -3,11 +3,30 @@
 // for tsükkel (üldtsükkel)
 
 
-let autod = ['Kia', 'Ford', 'Toyota', 'Nissan'];
-//väljastus
-for(let i = 0; i < autod.length; i++){
-    console.log(autod[i]);
+let numbrid = [];
+// lisame nimekirja juhuslikud arvud
+for(let i = 0; i < 10; i++){
+    let arv = Math.floor(Math.random() * 10);
+    numbrid.push(arv);
 }
+
+
+// kontrollime tulemust
+console.log(numbrid);
+// sorteerime kasvujärjekorras
+// numbrid.sort
+for (let kord = 0; kord < numbrid.length; kord++){
+    for(let i = 0; i < numbrid.length; i++){
+    let abi;
+    if(numbrid[i] > numbrid[i+1]){
+        abi = numbrid[i];
+        numbrid[i] = numbrid [i+1];
+        numbrid[i+1] = abi;
+    }
+  }
+}
+console.log(numbrid);
+
 
 
 
