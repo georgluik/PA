229925a99268bbbfafe5e269f6440bf164539729
georg.luik <georg.luik@khk.ee>
2019-10-28 10:54:23 +0200

    arvtsükkel (koos paaris-paaritu)

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // Tsüklid
 
 // for tsükkel (üldtsükkel)
-let arv;
-for(arv = 1; arv <= 10; arv++){
+
+
+let lause;
+for(let arv = 1; arv <= 10; arv++){
+    lause ='arv= ';
     if(arv < 10){
-        console.log('arv =  ' + arv);
-    } else{
-        console.log('arv = ' + arv);
-}
+        lause = lause + ' ';
+    }
+    lause = lause + arv;
+    if(arv % 2 == 0){
+        lause = lause + ' - paaris';
+    } else {
+        lause = lause + ' - paaritu';
+    }
+    console.log(lause);
 }
 
 
