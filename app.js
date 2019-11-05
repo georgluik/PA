575270b837c40e3e72bc52187f30a9fcecd6b9fc
georg.luik <georg.luik@khk.ee>
2019-11-05 14:40:34 +0200

    paarsuse kontroll

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-// Tsüklid
+// Funktsioonid
 
-// while
-// ellkontrolliga tsükkel
-let arv = 1; // defineerime tsükli juhtimismuutja
-// paneme kontrolltingimuse paika
-while(number <= 10){
-    // sooritame tegevused
-    console.log(number);
-    // suurendame juhtimismuutuja väärtus
-    number++;
-}
+// arvu paarsuse kontroll
+
+// funktsiooni defineerimine
+/* function funktsiooniNimi(param1, param2, .., paramN){
+    funktsioonisisu sisu 
+    } 
+*/
+function paarsuseKontroll(){
 
 
-//defineerime number
-let arv = 5;
-// defineerime jagaja algväärtus
-let jagaja = 2; 
-// suurendame jagaja väärtust nii kaua kuni jagamise jääk on 0
-while(arv % jagaja !=0){
-    jagaja++;
-}
-// kontrollime kas jagasime sama arvuga
-if(arv == jagaja){
-    console.log(arv + ' on algarv');
-    }    else {
-        console.log(arv + ' ei ole algarv');
+    // defineerime suvaline täisarv
+    let arv = 7;
+    // arvutame jääk 2-ga jagamisel
+    let jaak = arv % 2;
+    // kui jaak on võrdne nulliga
+    if (jaak === 0){
+        // tegu on paaris arvuga
+        console.log(arv + ' on paaris');
     }
+    else {
+        // tegu on paaritu arvuga
+        console.log(arv + ' on paaritu')
+    }
+}
 
 
-
-
+// funktsiooni kasutamine
+paarsuseKontroll();
 
     
