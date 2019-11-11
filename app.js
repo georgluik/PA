c0c8304181d77af0a0fe456baa4a02f0755c5517
georg.luik <georg.luik@khk.ee>
2019-11-11 10:31:21 +0200

    scope ploki põhine

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ var a = 1;
 var b = 2;
 const c = 3;
 
+// ploki põhine
+if(true){
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Plokk: ', a, b, c);
+}
+
+for(var a = 0; a < 10; a++){
+    console.log('Tsükkel: ', a);
+}
+
 console.log('Globaalne: ', a, b, c);
 
-    
+     
