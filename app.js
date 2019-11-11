6b19f838605c0b93a10888770cdb12a67074e9e4
georg.luik <georg.luik@khk.ee>
2019-11-11 10:27:16 +0200

    scope

--- app.js
+++ app.js
@@ -1,35 +1,10 @@
-// Funktsioonid
+// Nähtavus
 
-// arvu paarsuse kontroll
+// globaalne
+var a = 1;
+var b = 2;
+const c = 3;
 
-// funktsiooni defineerimine
-
-/* function funktsiooniNimi(param1, param2, .., paramN){
-    funktsioonisisu sisu 
-    } 
-*/
-// funktsioon mis arvutab ristküliku pindala
-function ristkylikuPindala(pikkus, laius){
-    
-
-    // arvutame pindala
-    let pindala = pikkus * laius;
-    
-    // väljastame pindala põhiprogrammi
-    return pindala;
-}
-
-// funktsioon mis väljastab tulemuse ekraanil
-function valjasta(pikkus = 0, laius = 0){
-    // väljastame väärtused
-    console.log('pikkus = ' + pikkus);
-    console.log('laius = ' + laius);
-    // salvestame arvutatud pindala
-    let pindala = ristkylikuPindala(pikkus,laius);
-    // väljastame tulemus
-    console.log('pindala = ' + pindala);
-}
-
-valjasta(15, 6);
+console.log('Globaalne: ', a, b, c);
 
     
